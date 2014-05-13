c44f5cfcc45eebba8b0c899f508303afc92d07d7
Steve Ayers <smaye81@gmail.com>
2014-05-13 14:05:45 -0400

    Karma configuration

---
+++ test-main.js
@@ -0,0 +1,40 @@
+var allTestFiles = [];
+var TEST_REGEXP = /(spec|test)\.js$/i;
+
+var pathToModule = function (path) {
+    "use strict";
+    return path.replace(/^\/base\//, '').replace(/\.js$/, '');
+};
+
+Object.keys(window.__karma__.files).forEach(function (file) {
+    "use strict";
+    if (TEST_REGEXP.test(file)) {
+        // Normalize paths to RequireJS module names.
+        allTestFiles.push(pathToModule(file));
+    }
+});
+
+require.config({
+    // Karma serves files under /base, which is the basePath from your config file
+    baseUrl: '/base',
+
+    paths: {
+        "angular": "node_modules/angular/lib/angular.min"
+    },
+
+    shim: {
+        'angular': {
+            exports: 'angular'
+        },
+        'angular-mocks': {
+            deps: ['angular'],
+            exports: 'angular.mock'
+        }
+    },
+
+    // dynamically load all test files
+    deps: allTestFiles,
+
+    // we have to kickoff jasmine, as it is asynchronous
+    callback: window.__karma__.start
+});
--- test/json2js_test.js
+++ test/json2js_test.js
@@ -1,3 +1,21 @@
-/**
- * Created by ayers on 5/13/14.
- */
+define(function (require) {
+
+    "use strict";
+
+    describe('HeaderCtrl', function () {
+        var angular = require('angular');
+        var mocks = require('angular-mocks');
+        var sut;
+
+        beforeEach(mocks.module('navigation'));
+
+        beforeEach(mocks.inject(function ($controller) {
+            sut = $controller('HeaderCtrl');
+        }));
+
+        it('should pass', function () {
+
+            return true;
+        })
+    });
+});
